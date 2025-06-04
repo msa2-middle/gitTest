c4a858760ec6506f9537c739496186a713853ea4
Patrick <jjgus29@naver.com>
2025-06-04 12:02:00 +0900

    feat: test

---
+++ team2/src/test/test.java
@@ -0,0 +1,12 @@
+package test;
+
+public class test {
+
+    public static void output() {
+        System.out.println("test");
+    }
+
+    public static void main(String[] args) {
+        output();
+    }
+}
