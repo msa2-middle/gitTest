0a571931f1e7c624bfd76111b45b2c6c21d3677f
YeeunWoo <107892669+YeeunWoo@users.noreply.github.com>
2025-06-04 12:12:11 +0900

    feat: yeeun.java 생성

--- team2/src/yeeun/yeeun.java
+++ team2/src/yeeun/yeeun.java
@@ -2,6 +2,6 @@ package yeeun;
 
 public class yeeun {
     public static void main(String[] args) {
-        System.out.println("브랜치 연습 중입니다!");
+        System.out.println("브랜치 연습 중입니다!!!");
     }
 }
